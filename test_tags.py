c3de4ae5204a83d2b99cede87284f4807ccc42fa
madhumithadasarathy <madhumithadasarathy@gmail.com>
2025-07-26 20:00:14 +0530

    Created test_tags.py to test the tagger

---
+++ test_tags.py
@@ -0,0 +1,9 @@
+from src.tagger import extract_tags
+
+sample_text = """
+Generative AI like ChatGPT and BERT are trained on huge datasets. These models are changing how content is written in companies like Google and Microsoft.
+"""
+
+tags = extract_tags(sample_text)
+
+print("🔖 Extracted Tags:", tags)
