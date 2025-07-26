0cc79fb9be577a839dcac358687503e44d046083
madhumithadasarathy <madhumithadasarathy@gmail.com>
2025-07-26 21:03:34 +0530

    Updated the location of the tagger

--- test_tags.py
+++ test_tags.py
@@ -1,4 +1,4 @@
-from src.tagger import extract_tags
+from genbrain_core.tagger import extract_tags
 
 sample_text = """
 Generative AI like ChatGPT and BERT are trained on huge datasets. These models are changing how content is written in companies like Google and Microsoft.
